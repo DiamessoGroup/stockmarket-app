280ecb5f66d4a72e4524e14d2e679aa8e10ddb9f
Krishna Diamesso <krishnadiamesso@gmail.com>
2021-05-24 18:51:12 -0500

    Added ENV variable for stock api key

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
 class ApplicationController < ActionController::Base
-  #StockQuote::Stock.new(api_key: "#{ENV["stock_api_key"]}")
-  StockQuote::Stock.new(api_key: "pk_cd2182c6368f4203a3bfd7d7511c4faa")
+  StockQuote::Stock.new(api_key: "#{ENV["stock_api_key"]}")
 end
