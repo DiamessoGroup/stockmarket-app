ff858afe822fded364aff66c5c2e64f64607f0f7
Krishna Diamesso <krishnadiamesso@gmail.com>
2021-05-24 18:31:20 -0500

    testing production key

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
-  StockQuote::Stock.new(api_key: "#{ENV["stock_api_key"]}")
+  #StockQuote::Stock.new(api_key: "#{ENV["stock_api_key"]}")
+  StockQuote::Stock.new(api_key: "pk_cd2182c6368f4203a3bfd7d7511c4faa")
 end
